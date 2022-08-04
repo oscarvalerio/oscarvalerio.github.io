69128b100c9f5c4a676432be79c66cf099a9d03b
oscarvalerio <oscarvalerio@gmail.com>
2022-08-03 20:30:20 -0500

    Adding logic to generate a random username and compare it to username field

--- seleniumExercises/js/practice1.js
+++ seleniumExercises/js/practice1.js
@@ -1,24 +1,35 @@
-console.log('this is working');
+let rndm=0;
+// This is the functionality of the submit button
+const sndBtn = document.querySelector('#sendCode');
+sndBtn.onclick = function(){
+    generateRandomMessage();
+}
+
 
 // These lines of code prevent the form to be submitted after clicking on the button
 const formas = document.querySelector('#loginForm');
 function doForm(e) {
     e.preventDefault();
-    catchValue();
-    generateRandomMessage();
+    const usr = catchValue();
+    if(usr==rndm){
+        console.log("welcome "+usr);
+    } else{
+        console.log("sorry you're not allowed to login")
+    }
+    
 }
 
 // This function catches the value of the username and password fields from the form
 function catchValue() {
     const usrField = document.querySelector("#userField").value;
-    const pswField = document.querySelector("#passwordField").value;
-    console.log(usrField);
-    console.log(pswField);
+    return usrField;
 }
 
+// Generates a random username and then returns the value
 function generateRandomMessage() {
     const alrtField = document.querySelector("#textAlert");
-    alrtField.classList.toggle('invField')
+    rndm = Math.floor(Math.random() * 100000);
+    alrtField.innerText = `The username you have to type is:  ${rndm}`;
 }
 
 
